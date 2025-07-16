07a26335d5a030100d59be98510dc0b7bee189f9
JoeA108 <212953767+JoeA108@users.noreply.github.com>
2025-07-16 14:26:11 +0000

    added stuff

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    if "excel_file" not in request.files:
+        return redirect(url_for("index"))
+
+    file = request.files["excel_file"]
+
+    if file.filename == "":
+        return redirect(url_for("index"))
+
+    if file and file.filename.lower().endswith((".xlsx", ".xls")):
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("index")) 
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000)
 
 
